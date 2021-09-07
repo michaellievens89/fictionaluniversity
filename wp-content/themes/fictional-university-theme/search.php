ae2300522a6ff07b2d9ef4298a9fa8713f4662ce
Michael Lievens <michaellievens89@gmail.com>
2021-09-07 16:20:30 +0200

    traditional search v3

---
+++ wp-content/themes/fictional-university-theme/search.php
@@ -0,0 +1,35 @@
+<?php 
+
+get_header();
+pageBanner(array(
+  'title' => 'Search Results',
+  'subtitle' => 'You searched for &ldquo;' . esc_html(get_search_query(false)) . '&rdquo;'
+)); 
+?>
+
+<div class="container container--narrow page-section">
+<?php
+  while(have_posts()) {
+    the_post();
+    get_template_part('template-parts/content', get_post_type()); 
+    ?>
+    <div class="post_item">
+      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <div class="metabox">
+        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.Y'); ?> in <?php echo get_the_category_list(', ') ?></p>
+      </div>
+
+      <div class="generic-content">
+        <?php the_excerpt(); ?>
+        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+        <br>
+      </div>
+    </div>
+  <?php }
+  echo paginate_links();
+?>
+</div>
+
+<?php get_footer();
+
+?>
